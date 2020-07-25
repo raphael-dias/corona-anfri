fbafc25cce16d38047ad3a089ff32e1eede51547
Raphael Dias <raphael.cdias@outlook.com>
2020-07-25 14:06:48 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Teste</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
